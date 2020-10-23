7ea5a610b22012563c92fc5ad7207e8a62728754
ahmedsujon <ahmedsujon050@gmail.com>
2020-10-23 16:05:44 +0600

    Laravel8 MultiAuth

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -8,22 +8,15 @@ use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  ...$guards
-     * @return mixed
-     */
-    public function handle($request, Closure $next, ...$guards)
-    {
-        $guards = empty($guards) ? [null] : $guards;
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if ($guard == "admin" && Auth::guard($guard)->check()) {
+            return redirect()->route('admin.dashboard');
+        }
+
+        if (Auth::guard($guard)->check()) {
+            return redirect()->route('home');
         }
 
         return $next($request);
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,16 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',  'WelcomeController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Admin routes
+Route::prefix('admin')->group(function(){
+    Route::get('/', 'Users\Admin\AdminController@index')->name('admin.dashboard');
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/register', 'Auth\AdminRegisterController@showRegisterForm')->name('admin.register');
+    Route::post('/register', 'Auth\AdminRegisterController@register')->name('admin.register.submit');
+});
